89a4f19f6ca4d42d7f563a830e29c0c6f62ee85e
cwx <1441987849@qq.com>
2016-09-16 22:52:46 +0800

    unfinished

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "1.0"
 scalaVersion := "2.11.8"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.0.0"
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.0.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.0.0"
--- src/main/scala/LR.scala
+++ src/main/scala/LR.scala
@@ -1,16 +1,53 @@
+// General purpose library
+import scala.xml._
+
+// Spark data manipulation libraries
+import org.apache.spark.sql.catalyst.plans._
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+
+// Spark machine learning libraries
+import org.apache.spark.ml.feature.{HashingTF, Tokenizer}
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.ml.Pipeline
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
- 
+
 object LR {
-    def main(args: Array[String]) {
-        val logFile = "file:///usr/local/spark/README.md" // Should be some file on your system
-        val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
-        val sc = new SparkContext(conf)
-        val logData = sc.textFile(logFile, 2).cache()
-        val numAs = logData.filter(line => line.contains("a")).count()
-        val numBs = logData.filter(line => line.contains("b")).count()
-        println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
-        sc.stop   
-    }
+	def main(args: Array[String]) {
+		val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
+				val sc = new SparkContext(conf)
+		val fileName = "/home/hadoop/data/sparkdata/Posts.small.xml"
+		val textFile = sc.textFile(fileName)
+		val postsXml = textFile.map(_.trim).
+		filter(!_.startsWith("<?xml version=")).
+		filter(_ != "<posts>").
+		filter(_ != "</posts>")
+		val postsRDD = postsXml.map { s =>
+		val xml = XML.loadString(s)
+
+		val id = (xml \ "@Id").text
+		val tags = (xml \ "@Tags").text
+
+		val title = (xml \ "@Title").text
+		val body = (xml \ "@Body").text
+		val bodyPlain = ("<\\S+>".r).replaceAllIn(body, " ")
+		val text = (title + " " + bodyPlain).replaceAll("\n", 
+				" ").replaceAll("( )+", " ");
+
+		println(id + "cwx")
+		Row(id, tags, text)
+		}
+		val schemaString = "Id Tags Text"
+				val schema = StructType(
+						schemaString.split(" ").map(fieldName => 
+						StructField(fieldName, StringType, true)))
+						
+            val sqlContext = new SQLContext(sc)
+						val postsDf = sqlContext.createDataFrame(postsRDD, schema)
+						sc.stop   
+	}
 }
