03425856dfa1fce34080f1d77a218a8cda90fcd9
cwx <1441987849@qq.com>
2016-09-17 15:02:05 +0800

    modify train and test data

--- src/main/scala/LR.scala
+++ src/main/scala/LR.scala
@@ -20,13 +20,11 @@ object LR {
 	def main(args: Array[String]) {
 		val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
 				val sc = new SparkContext(conf)
-		val fileName = "/home/hadoop/data/sparkdata/Posts.xml"
 
+		val fileName = "/home/hadoop/data/sparkdata/Posts.xml"
 		val textFile = sc.textFile(fileName)
-		val postsXml = textFile.map(_.trim).
-		filter(!_.startsWith("﻿<?xml version=")).
-		filter(_ != "<posts>").
-		filter(_ != "</posts>")
+		val postsXml = textFile.map(_.trim).filter(!_.startsWith("﻿<?xml version=")).filter(_ != "<posts>").filter(_ != "</posts>")
+
 		val postsRDD = postsXml.map { s =>
 		val xml = XML.loadString(s)
 		val id = (xml \ "@Id").text
@@ -34,47 +32,32 @@ object LR {
 		val title = (xml \ "@Title").text
 		val body = (xml \ "@Body").text
 		val bodyPlain = ("<\\S+>".r).replaceAllIn(body, " ")
-		val text = (title + " " + bodyPlain).replaceAll("\n", 
-				" ").replaceAll("( )+", " ");
+		val text = (title + " " + bodyPlain).replaceAll("\n", " ").replaceAll("( )+", " ");
 		Row(id, tags, text)
 		}
 
 		val schemaString = "Id Tags Text"
-				val schema = StructType(
-						schemaString.split(" ").map(fieldName => 
-						StructField(fieldName, StringType, true)))
-						val sqlContext = new SQLContext(sc)
+				val schema = StructType(schemaString.split(" ").map(fieldName => StructField(fieldName, StringType, true)))
+				val sqlContext = new SQLContext(sc)
 		val postsDf = sqlContext.createDataFrame(postsRDD, schema)
 
 		val targetTag = "java"
-		val myudf: (String => Double) = (str: String) => 
-		{if (str.contains(targetTag)) 1.0 else 0.0}
+		val myudf: (String => Double) = (str: String) => {if (str.contains(targetTag)) 1.0 else 0.0}
 		val sqlfunc = udf(myudf)
-				val postsLabeled = postsDf.withColumn("Label", 
-						sqlfunc(col("Tags")) )
+				val postsLabeled = postsDf.withColumn("Label", sqlfunc(col("Tags")) )
 
-						val positive = postsLabeled.filter(postsLabeled("Label") > 0.0)
-						val negative = postsLabeled.filter(postsLabeled("Label") < 1.0)
-						val positiveTrain = positive.sample(false, 0.9)
-						val negativeTrain = negative.sample(false, 0.9)
-						val training = positiveTrain.union(negativeTrain).show()
+				val positive = postsLabeled.filter(postsLabeled("Label") > 0.0)
+				val negative = postsLabeled.filter(postsLabeled("Label") < 1.0)
+				val positiveTrain = positive.sample(false, 0.9)
+				val negativeTrain = negative.sample(false, 0.9)
+				val training = positiveTrain.union(negativeTrain).show()
 
-						val negativeTrainTmp = negativeTrain
-						.withColumnRenamed("Label", "Flag")
-						.select("Id", "Flag")
-						val negativeTest = negative.join( negativeTrainTmp, 
-								negative("Id") === negativeTrainTmp("Id"), 
-								"LeftOuter").filter("Flag is null")
-								.select(negative("Id"), negative("Tags"), negative("Text"), negative("Label"))
-								val positiveTrainTmp = positiveTrain
-								.withColumnRenamed("Label", "Flag")
-								.select("Id", "Flag")
-								val positiveTest = positive.join( positiveTrainTmp, 
-										positive("Id") === positiveTrainTmp("Id"), 
-										"LeftOuter").filter("Flag is null")
-										.select(positive("Id"), positive("Tags"), positive("Text"), positive("Label"))
-										val testing = negativeTest.union(positiveTest).show()
+				val negativeTrainTmp = negativeTrain.withColumnRenamed("Label", "Flag").select("Id", "Flag")
+				val negativeTest = negative.join( negativeTrainTmp, negative("Id") === negativeTrainTmp("Id"),"LeftOuter").filter("Flag is null").select(negative("Id"), negative("Tags"), negative("Text"), negative("Label"))
+				val positiveTrainTmp = positiveTrain.withColumnRenamed("Label", "Flag").select("Id", "Flag")
+				val positiveTest = positive.join( positiveTrainTmp, positive("Id") === positiveTrainTmp("Id"), "LeftOuter").filter("Flag is null").select(positive("Id"), positive("Tags"), positive("Text"), positive("Label"))
+				val testing = negativeTest.union(positiveTest).show()
 
-										sc.stop   
+				sc.stop   
 	}
 }
